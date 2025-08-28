9846458203bee94ae67d01fe3ea3d2e91841337f
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-08-27 22:39:17 -0300

    :star: last update

--- tp1/main.py
+++ tp1/main.py
@@ -1,5 +1,6 @@
 #
-# TP1 - Aragon Joaquin
+# TP1 - Aragon Joaquin, Gómez Nicolas Miguel,
+#       Marraccini Daniel, Vazquez Federico. 
 # 
 
 import pandas as pd
@@ -10,13 +11,8 @@ from colorPrinter import ColorPrint, COLORS
 # 1)
 dtSet = pd.read_csv('./mock/dataset2.csv') # leemos el .csv desde mocks
 
-# 1.5) Transformamos a sus respectivos tipos
-
-dtSet[['Ingresos', 'Edad']] = dtSet[['Ingresos', 'Edad']].apply(pd.to_numeric, errors='coerce')  # transformamos campo a numero para luego ordernalo y suprimimos erorres
-dtSet = dtSet.astype({"Nombre": str, "Ciudad": str, "Ocupacion": str})
-
 # 2)
-ColorPrint(dtSet.head(4), COLORS.GREEN) # primeras 4 filas printeadas
+ColorPrint(dtSet.head(4), COLORS.GREEN) # primeras 4 filas printeada
 
 ColorPrint(dtSet.info(), COLORS.GREEN) # printea el informacion sobre el DataSet (no nulos, tipo de dato, columna)
 
@@ -24,27 +20,27 @@ ColorPrint(format(f"Filas x Columnas: {dtSet.shape}"), COLORS.GREEN) #printea di
 
 ColorPrint(format(f" Descripcion:\n{dtSet.describe()}"), COLORS.GREEN) #printea descripcion general (min, max, standard, media, etc...)
 
+#2.5) Transformamos a sus respectivos tipos
+dtSet[['Ingresos', 'Edad']] = dtSet[['Ingresos', 'Edad']].apply(pd.to_numeric, errors='coerce')  # transformamos campo a numero para luego ordernalo y suprimimos erorres
+dtSet = dtSet.astype({"Nombre": str, "Ciudad": str, "Ocupacion": str})
+
 # 3)
 ColorPrint(format(f"3.1) DETECTAR NULOS POR CAMPO:\n{dtSet.isnull().sum()}"), COLORS.RED) # printea true si el campo es nulo
 
-edadMedia = round(dtSet["Edad"].mean()) # agarramos la media y la redondeamos
-ingresoMedia = round(dtSet["Ingresos"].mean()) # x2 aca
+edadMedia = dtSet["Edad"].mean() # calculamos la media de edad
+ingresoMediana = dtSet["Ingresos"].median() #calculamos la mediana de ingresos
 
-# dtSet["Ingresos"].fillna(0, inplace = True) #cambiamos ingresos a 0 modificando el dataSet Previo (inplace=true)
-# dtSet["Edad"].fillna(0, inplace = True) #mismo para edad
-dtSet.fillna({"Ingresos": ingresoMedia, "Edad": edadMedia}, inplace=True) # pero mejor en una sola linea! (y haciendolo con la media!)
+dtSet.fillna({"Ingresos": ingresoMediana, "Edad": edadMedia}, inplace=True) #usando la media y mediana respectivamente
 
 ColorPrint(format(f"3.2) RELLENAR VALORES nan!:\n{dtSet}"), COLORS.RED)
 
-# 5) (cambie de orden con el 4 por inconsistencias de 0 y "?")
+# 4)
+prevDups = dtSet.duplicated().sum()
+dtSet.drop_duplicates(inplace=True) # modificamos el dataset
+ColorPrint(format(f"4) BORRAR DUPLICADOS!:\n{prevDups} han sido borrados"), COLORS.YELLOW)
 
-# Linea de abajo es irrelevante debido a que no existen datos "?" / "desconocido" por la conversion del punto 1.5 (aunque con errors="ignore" si se podria llegar a este caso, pero por el bien de no añadir complejidad innecesaria, sera de esta forma)
-
-#dtSet.replace({"Ingresos": "?", "Edad": "desconocido" }, {"Ingresos": ingresoMedia, "Edad": edadMedia}, inplace=True)
-#ColorPrint(format(f"4) REEMPLAZAR INCOSISTENCIAS '?' Y 'desconocido'!:\n{dtSet}"), COLORS.CYAN)
-
-ColorPrint(format(f"4.1) NUMERO DE VALORES NULOS EN LOS CAMPOS STR: \n{dtSet[['Nombre','Ciudad','Ocupacion']].isnull().sum()}"), COLORS.RED) 
-
+# 5) 
+ColorPrint(format(f"5.1) NUMERO DE VALORES NULOS EN LOS CAMPOS STR: \n{dtSet[['Nombre','Ciudad','Ocupacion']].isnull().sum()}"), COLORS.RED) 
 
 # dtSet["Nombre"].replace('nan', 'Anonimo')
 # dtSet["Ciudad"].replace('nan', 'Desconocido')
@@ -52,35 +48,25 @@ ColorPrint(format(f"4.1) NUMERO DE VALORES NULOS EN LOS CAMPOS STR: \n{dtSet[['N
 
 
 dtSet.replace({'Nombre': {pd.NA: 'Anonimo'}, 'Ciudad': {pd.NA: 'Desconocido'}, 'Ocupacion': {pd.NA: 'Desconocido'}}, inplace=True) #equivalente al bloque anterior
-ColorPrint(format(f"4.1) NUMERO DE VALORES LUEGO DE REEMPLAZAR NULOS: \n{dtSet[['Nombre','Ciudad','Ocupacion']].isnull().sum()}"), COLORS.RED) 
-# 4)
-
-prevDups = dtSet.duplicated().sum()
-dtSet.drop_duplicates(inplace=True) # modificamos el dataset
-ColorPrint(format(f"5) BORRAR DUPLICADOS!:\n{prevDups} han sido borrados"), COLORS.YELLOW)
+ColorPrint(format(f"5.1) NUMERO DE VALORES LUEGO DE REEMPLAZAR NULOS: \n{dtSet[['Nombre','Ciudad','Ocupacion']].isnull().sum()}"), COLORS.RED) 
 
 
 # 6)
 ColorPrint(format(f"6) ANALISIS DESCRIPTIVO:\n{dtSet.describe()}"), COLORS.MAGENTA) #analisis descriptivo
 
 # 7) 
-
-#  WARNING: 
-# Es posible que no funcione
-# https://stackoverflow.com/questions/77507580/userwarning-figurecanvasagg-is-non-interactive-and-thus-cannot-be-shown-plt-sh
-# SOLUCION: pip install PyQt6... y tener python 3.12+
-
 # 7.1) grafico de barras 
-dtIngresos = dtSet.sort_values('Ingresos', ascending=True)
+dtProm = dtSet.groupby('Nombre')['Ingresos'].mean().reset_index() # agrupamos por nombre y calculamos la media de sus ingresos, reseteamos el index para que quede prolijo
+dtProm.sort_values('Ingresos', ascending=True, inplace=True) # reescribrimos en dtProm y ordenamos desde menor a mayor
 
-plt.bar(dtIngresos['Nombre'], dtIngresos['Ingresos'], color='red')
+plt.bar(dtProm['Nombre'], dtProm['Ingresos'], color='red')
 plt.xlabel("Nombre", size=12)
 plt.ylabel("Ingresos", size=12)
 plt.title("Ingresos por persona", size=16)
 plt.grid(True)
 plt.show()
 
-# # 7.2) grafico de esparcimiento 
+# # 7.2) Grafico de esparcimiento 
 plt.scatter(dtSet['Ocupacion'],dtSet['Edad'])
 plt.xlabel("Ocupacion", size=12)
 plt.ylabel("Edad", size=12)
@@ -88,7 +74,7 @@ plt.title("Edad por Ocupacion", size=16)
 plt.grid(True)
 plt.show()
 
-# 7.3) grafico circular (pie chart)
+# 7.3) Grafico circular (pie chart)
 names = []
 timeAppeared = []
 
@@ -98,5 +84,5 @@ for name, count in dtSet['Nombre'].value_counts().items():
 
 fig, ax = plt.subplots()
 ax.pie(timeAppeared, labels=names, autopct='%1.1f%%')
-ax.set_title('Porcentaje de aparecimiento de nombres', size=16)
+ax.set_title('Porcentaje de aparicion de nombres', size=16)
 plt.show()
